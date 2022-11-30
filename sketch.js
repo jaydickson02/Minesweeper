2c03f25a516633e6e24dbe9db874b04f60e789e4
Jay Dickson <jaydickson02@gmail.com>
2022-12-01 00:16:48 +1100

    Add win/lose conditions

--- sketch.js
+++ sketch.js
@@ -5,13 +5,17 @@ let bombPercentage = 0.1; // Percentage
 let bombNumber = bombPercentage * gridHeight * gridWidth;
 let scale = 30; // Scale of the grid in px
 let grid = []; // Stores all grid elements
+let gameOver = false; //Sets gameover flag
+let gameFinished = false; //Sets game finished flag
 
-function windowResized() {
-    resizeCanvas(windowWidth - 100, windowHeight - 100);
-}
+let tempNumBombs = 0; //Temporary flag to track the number of bombs generated. Fix this.
+
+//function windowResized() {
+//    resizeCanvas(gridWidth * scale, gridHeight * scale);
+//}
 
 function setup() {
-    createCanvas(windowWidth - 100, windowHeight - 100);
+    createCanvas(gridWidth * scale + 1, gridHeight * scale + 1);
     frameRate(10);
 
     //Grid Generation variables
@@ -21,7 +25,8 @@ function setup() {
     //Generate Grid
     for (let i = 0; i < gridWidth; i++) {
         for (let j = 0; j < gridHeight; j++) {
-            if (Math.random() < bombPercentage && placedBombs < bombNumber - 1) {
+            //This should run untill there are definitly the correct number of bombs.
+            if (Math.random() < bombPercentage && placedBombs < bombNumber - 1) { 
                 type = "bomb";
                 placedBombs++
             } else {
@@ -29,6 +34,13 @@ function setup() {
             }
 
             grid.push(new GridElement(type, i, j, false, scale))
+        }
+    }
+
+    //Calculate the number of actual bombs placed. Fix this.
+    for(let cell of grid){
+        if(cell.type == "bomb"){
+            tempNumBombs++;
         }
     }
 
@@ -40,24 +52,61 @@ function setup() {
 
 //Register a mouse click on a given cell and set the clicked flag.
 function mousePressed() {
-    for (let i = 0; i < grid.length; i++) {
-        //Collision Detection
-        if (grid[i].position.x * scale < mouseX && grid[i].position.x * scale + scale > mouseX && grid[i].position.y * scale < mouseY && grid[i].position.y * scale + scale > mouseY) {
-            //grid[i].isClicked(true);
-            //grid[i].unlockSurroundingCells([]);
-            grid[i].bfs_function()
+    if(!gameOver){
+
+        
+            
+        
+
+        for (let i = 0; i < grid.length; i++) {
+            //Collision Detection
+            if (grid[i].position.x * scale < mouseX && grid[i].position.x * scale + scale > mouseX && grid[i].position.y * scale < mouseY && grid[i].position.y * scale + scale > mouseY) {
+
+
+                if (mouseButton == RIGHT) {
+                    grid[i].flag();
+                } else {
+                    grid[i].bfs_function();
+                }
+                
+            }
         }
     }
     return false;
 }
 
 function draw() {
-    
+    let cellsOpened = 0; 
     //Draw grid
 
     for (let element of grid){
         element.draw()
+        if(element.clicked && element.type == "bomb"){
+            gameOver = true;
+        }
+        if(element.type == "cell" && element.clicked == true){
+            cellsOpened++;
+        }
     }
+
+    if(cellsOpened == (grid.length - tempNumBombs)){ //Fix tempNumBombs
+        gameFinished = true;
+    }
+
+    if(gameOver == true){
+        for(let element of grid){
+            if(element.type == "bomb"){
+                element.isClicked(true);
+            }
+        }
+    }
+
+    if(gameFinished == true){
+        fill(0);
+        text("Success", width/2, height/2);
+    }
+
+    
 
 
     
